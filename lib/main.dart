679b5fb926097a4ce36055351c0f0bd90121c3da
wangAppPC <wanghatyai@gmail.com>
2019-08-08 14:09:32 +0700

    test signature with permission_handler

---
+++ lib/main.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+const directoryName = 'Signature';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  GlobalKey<SignatureState> signatureKey = GlobalKey();
+  var image;
+  String _platformVersion = 'Unknown';
+  //Permission _permission = Permission.WriteExternalStorage;
+
+  @override
+  void initState() {
+    super.initState();
+    //initPlatformState();
+  }
+  // Platform messages are asynchronous, so we initialize in an async method.
+  /*initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await SimplePermissions.platformVersion;
+
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+    print(_platformVersion);
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Signature(key: signatureKey),
+      persistentFooterButtons: <Widget>[
+        FlatButton(
+          child: Text('Clear'),
+          onPressed: () {
+            signatureKey.currentState.clearPoints();
+          },
+        ),
+        FlatButton(
+          child: Text('Save'),
+          onPressed: () {
+            // Future will resolve later
+            // so setState @image here and access in #showImage
+            // to avoid @null Checks
+            setRenderedImage(context);
+          },
+        )
+      ],
+    );
+  }
+
+  setRenderedImage(BuildContext context) async {
+    ui.Image renderedImage = await signatureKey.currentState.rendered;
+
+    setState(() {
+      image = renderedImage;
+    });
+
+    showImage(context);
+  }
+
+  Future<Null> showImage(BuildContext context) async {
+    var pngBytes = await image.toByteData(format: ui.ImageByteFormat.png);
+    //Map<PermissionGroup, PermissionStatus> _permission = await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    if(!(await checkPermission())) await requestPermission();
+    // Use plugin [path_provider] to export image to storage
+    Directory directory = await getExternalStorageDirectory();
+    String path = directory.path;
+    print(path);
+    await Directory('$path/$directoryName').create(recursive: true);
+    File('$path/$directoryName/${formattedDate()}.png')
+        .writeAsBytesSync(pngBytes.buffer.asInt8List());
+    return showDialog<Null>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(
+              'Please check your device\'s Signature folder',
+              style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w300,
+                  color: Theme.of(context).primaryColor,
+                  letterSpacing: 1.1
+              ),
+            ),
+            content: Image.memory(Uint8List.view(pngBytes.buffer)),
+          );
+        }
+    );
+  }
+
+  String formattedDate() {
+    DateTime dateTime = DateTime.now();
+    String dateTimeString = 'Signature_' +
+        dateTime.year.toString() +
+        dateTime.month.toString() +
+        dateTime.day.toString() +
+        dateTime.hour.toString() +
+        ':' + dateTime.minute.toString() +
+        ':' + dateTime.second.toString() +
+        ':' + dateTime.millisecond.toString() +
+        ':' + dateTime.microsecond.toString();
+    return dateTimeString;
+  }
+
+  requestPermission() async {
+    //bool result = await SimplePermissions.requestPermission(_permission);
+    //Map<PermissionGroup, PermissionStatus> result = await PermissionHandler.requestPermissions(_permission);
+    Map<PermissionGroup, PermissionStatus> result = await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+
+    return result;
+  }
+
+  checkPermission() async {
+
+    //PermissionStatus result = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    bool result = await PermissionHandler().shouldShowRequestPermissionRationale(PermissionGroup.storage);
+    //bool result = await SimplePermissions.checkPermission(_permission);
+    return result;
+  }
+
+  getPermissionStatus() async {
+
+    final result = await PermissionHandler().checkServiceStatus(PermissionGroup.storage);
+    //final result = await SimplePermissions.getPermissionStatus(_permission);
+    print("permission status is " + result.toString());
+  }
+}
+
+class Signature extends StatefulWidget {
+  Signature({Key key}): super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return SignatureState();
+  }
+}
+
+class SignatureState extends State<Signature> {
+  // [SignatureState] responsible for receives drag/touch events by draw/user
+  // @_points stores the path drawn which is passed to
+  // [SignaturePainter]#contructor to draw canvas
+  List<Offset> _points = <Offset>[];
+
+  Future<ui.Image> get rendered {
+    // [CustomPainter] has its own @canvas to pass our
+    // [ui.PictureRecorder] object must be passed to [Canvas]#contructor
+    // to capture the Image. This way we can pass @recorder to [Canvas]#contructor
+    // using @painter[SignaturePainter] we can call [SignaturePainter]#paint
+    // with the our newly created @canvas
+    ui.PictureRecorder recorder = ui.PictureRecorder();
+    Canvas canvas = Canvas(recorder);
+    SignaturePainter painter = SignaturePainter(points: _points);
+    var size = context.size;
+    painter.paint(canvas, size);
+    return recorder.endRecording()
+        .toImage(size.width.floor(), size.height.floor());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox _object = context.findRenderObject();
+              Offset _locationPoints = _object.localToGlobal(details.globalPosition);
+              _points = new List.from(_points)..add(_locationPoints);
+            });
+          },
+          onPanEnd: (DragEndDetails details) {
+            setState(() {
+              _points.add(null);
+            });
+          },
+          child: CustomPaint(
+            painter: SignaturePainter(points: _points),
+            size: Size.infinite,
+          ),
+        ),
+      ),
+    );
+  }
+
+  // clearPoints method used to reset the canvas
+  // method can be called using
+  //   key.currentState.clearPoints();
+  void clearPoints() {
+    setState(() {
+      _points.clear();
+    });
+  }
+}
+
+
+class SignaturePainter extends CustomPainter {
+  // [SignaturePainter] receives points through constructor
+  // @points holds the drawn path in the form (x,y) offset;
+  // This class responsible for drawing only
+  // It won't receive any drag/touch events by draw/user.
+  List<Offset> points = <Offset>[];
+
+  SignaturePainter({this.points});
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.square
+      ..strokeWidth = 5.0;
+
+    for(int i=0; i < points.length - 1; i++) {
+      if(points[i] != null && points[i+1] != null) {
+        canvas.drawLine(points[i], points[i+1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(SignaturePainter oldDelegate) {
+    return oldDelegate.points != points;
+  }
+
+}
